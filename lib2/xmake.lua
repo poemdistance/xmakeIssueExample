7b51e07db7912df731bc7d8266ea44c1082e9540
poemdistance <poemdistance@gmail.com>
2024-07-14 20:02:31 +0800

    using package

--- lib2/xmake.lua
+++ lib2/xmake.lua
@@ -1,8 +1,9 @@
 add_rules("mode.debug", "mode.release")
 
-includes("baseLib")
+add_repositories("local-repo build")
+add_requires("baseLib")
 
 target("lib2")
     set_kind("static")
     add_files("mul.c")
-    add_deps("baseLib")
+    add_packages("baseLib")
